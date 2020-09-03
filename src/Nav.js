b07d17a9469abe3f033341c602d75fae16578e9d
Bryan J Chasko <bryan@bryans-air.lan>
2020-09-03 12:56:55 -0600

    Finished with the chapter. Still needs review.

---
+++ src/Nav.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Menu } from "antd";
+import {
+  HomeOutlined,
+  ProfileOutlined,
+  FileProtectOutlined,
+} from "@ant-design/icons";
+
+const Nav = (props) => {
+    const { current } = props
+    return (
+        <div>
+            <Menu selectedKeys={[current]} mode="horizontal">
+                <Menu.Item key="home">
+                    <Link to={`/`}>
+                        <HomeOutlined />Home
+                    </Link>
+                </Menu.Item>
+                <Menu.Item key="profile">
+                    <Link to="/profile">
+                    <ProfileOutlined/>Profile 
+                    </Link>
+                </Menu.Item>
+                <Menu.Item key="protected">
+                    <Link to="/protected">
+                        <FileProtectOutlined />protected
+                        </Link>
+                </Menu.Item>
+            </Menu>
+        </div>
+    )
+}
+export default Nav
---
+++ src/Public.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Container from "./Container"
+
+function Public() {
+    return (
+        <Container>
+            <h1>Public Route</h1>
+        </Container>
+    )
+}
+
+export default Public
